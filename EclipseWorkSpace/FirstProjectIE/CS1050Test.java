e349c55a3a0853dfa06a93993dbefa9e2cbaedd4
ellaevelynn <ieaton@msudenver.edu>
2026-01-22 13:59:21 -0700

    Test documents java
    initial test docs created using the getting started doc

---
+++ EclipseWorkSpace/FirstProjectIE/CS1050Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class CS1050Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// print method
+		System.out.print("Hello Java");
+
+	}
+
+}
