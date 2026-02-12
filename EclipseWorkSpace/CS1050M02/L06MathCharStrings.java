3f0bb044697608d75ef7e99e3626892112762165
ellaevelynn <ieaton@msudenver.edu>
2026-02-12 11:56:39 -0700

    Update L06MathCharStrings.java
    editing charstrings in class

--- EclipseWorkSpace/CS1050M02/L06MathCharStrings.java
+++ EclipseWorkSpace/CS1050M02/L06MathCharStrings.java
@@ -5,12 +5,13 @@
 /**
  * 
  */
-import java.util.Scanner;
+import java.util.Scanner; //allows scanner to be used in program
 public class L06MathCharStrings
 {
 	
 	public static void main(String[] args)
 	{
+		//exercise 1
 // What is happening for each line?
 double randomDouble = Math.random();
 //produce random double value 
@@ -20,6 +21,7 @@ randomInt = (int) (Math.random() * 10);
 //random value *10, truncate decimal to make integer 
 randomInt = (int) (Math.random() * 10 + 1);
 //random value *10+1 , truncate decimal to make integer
+	//exercise 2 
 char charTest= 'a';
 int charAsciiValue= (int)charTest;
 String firstName = "Herbierto";
@@ -27,6 +29,7 @@ System.out.printf("char: %c ascii value: %d \n", charTest, charAsciiValue);
 //when using printf: save space and put variables in after the strong 
 //println: variables can catinate 
 //prinitive vs non primitive
+		//Exercise 3 
 System.out.println("Hello " + firstName);
 int stringLength = firstName.length();
 char firstInitial = firstName.charAt(0);
@@ -34,6 +37,7 @@ char lastCharacter=firstName.charAt((firstName.length()-1));
 System.out.println(lastCharacter);
 //alternative method 
 char lastLetter= firstName.charAt(stringLength-1);
+// Last class example (stopped here, add more comments)
 Scanner input = new Scanner(System.in);
 System.out.println(lastLetter);
 System.out.println("Enter first name");
