f347d356259022adb109725d718a46e6c6f29574
ellaevelynn <ieaton@msudenver.edu>
2026-02-17 13:44:53 -0700

    gemo2 work

---
+++ EclipseWorkSpace/CS1050M02/IEMGE02MathCharsStrings.java
@@ -0,0 +1,60 @@
+/**
+ * 
+ */
+
+/** This program creates a short Mad Lib story 
+ */
+import java.util.Scanner; //allows scanner to be used in program
+public class IEMGE02MathCharsStrings
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		// declare and initialize string variables that will later be edited when your program is ran
+		 String firstName="name";
+		 String lastName ="last";
+		 String nounAny= "noun";
+		 String ingVerb="verb";
+		 String adjectiveAny="adjective";
+		 String placeAny="place";
+		 String favoriteLetter="A";
+		 
+		// incorperate scanner so that keyboard inputs can be used 
+		 Scanner input = new Scanner(System.in); //call scanner to use keyboard inputs 
+		 
+		//fill-in-the-blank for your MadLib, replace initial values for variables with keyboard inputs
+		 System.out.println("Let's create a MadLib!");
+		 System.out.println("Enter a first name:");
+		 firstName = input.next();
+		 System.out.println("Enter a last name:");
+		 lastName = input.next();
+		//extract first character of the name and store it as a char 
+		 char lastNameLastInitial = input.next().charAt(0);
+		 char lastName=lastNameLastInitial;
+		 
+		 System.out.println("Enter a noun (a person, place or thing):");
+		 nounAny = input.next();
+		 System.out.println("Enter a verb (action word) ending in -ing:");
+		 ingVerb = input.next();
+		 System.out.println("Enter an adjective (a descriptive word):");
+		 adjectiveAny = input.next();
+		 System.out.println("Enter a place:");
+		 placeAny = input.next();
+		 System.out.println("Enter your favorite letter:");
+		 favoriteLetter = input.next();
+		 
+		
+		//Print the final completed  MadLib 
+		 String summary = "Once upon a time " + firstName + " " + lastName + "was"
+		 		+ ingVerb+" through the "+ placeAny+ "with a "+ nounAny+". "+ "It was a very "
+				 +adjectiveAny+ "day. Their favorite letter was "+ favoriteLetter +".";
+		 System.out.println(summary);
+		 input.close();	
+
+	}
+
+}
