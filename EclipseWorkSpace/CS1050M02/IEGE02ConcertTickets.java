d49b4a0e784501d4f1d8038f18c2390256e3f420
ellaevelynn <ieaton@msudenver.edu>
2026-02-24 13:50:31 -0700

    editing in class

---
+++ EclipseWorkSpace/CS1050M02/IEGE02ConcertTickets.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class IEGE02ConcertTickets
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
