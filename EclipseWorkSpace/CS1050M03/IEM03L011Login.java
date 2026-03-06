1f55934fb2d384b3ab4abd92ff1a101486319d97
ellaevelynn <ieaton@msudenver.edu>
2026-03-06 16:13:04 -0700

    editing

--- EclipseWorkSpace/CS1050M03/IEM03L011Login.java
+++ EclipseWorkSpace/CS1050M03/IEM03L011Login.java
@@ -6,56 +6,39 @@ public class IEM03L011Login
 	{
 	//scenario 1: user puts in the correct user name and correct password 
 		//storing correct use name and password as strings
+		final String CORRECT_USERNAME = "Test";
+		final String CORRECT_PASSWORD = "p@$$";
+		
+		Scanner input = new Scanner(System.in);
+		boolean loggedIn = false; // boolean flag
+		int loginAttempts=0; //storing login attempts
+		
+		System.out.println("Enter username and password to log in securely.");
+		do{
+			System.out.print("Enter username: ");
+			String username = input.nextLine().trim();
+			if (username.equals(CORRECT_USERNAME)){
+				System.out.println("Correct username.");
+				System.out.print("Enter password: ");
+				String password = input.next();
+			//gets username correct, moved on to prompt for password.
+				if (password.equals(CORRECT_PASSWORD)){
+					System.out.println("Login successful! Welcome.");
+					loggedIn = true; // set flag to exit loop
+				} else{
+					System.out.println("Incorrect password.");
+					loginAttempts++;
+				}
+			} else{
+				System.out.println("Incorrect username");
+				loginAttempts++;
+			}
+		} while (loggedIn == false && loginAttempts <3);
+		if (loginAttempts>=3) {
+			System.out.println("You are locked out.");
+		}
+		input.close();
 	
-	String correctStoredUsername="Test";
-	String correctStoredPassword="p@$$";
-	
-	//declaring and initializing password attempts
-	String usernameAttempt="test";
-	String passwordAttempt="passtest";
-	
-	Scanner input = new Scanner(System.in);//call scanner for user input 
-	
-	//replace placeholder string values  for user name and password attempts with user input 
-	System.out.println("Enter your username:");
-	usernameAttempt= input.next();
-	//cant get to password without getting a correct username!
-	//for loop not good, not always runnning 3 times/iterations
-	//don't have to worry about first 3 scenarios, have to think about algorithm notes 
-	//1 thing I need to know in order to repeat
-	//need to keep track of attempts (start at 0)
-	//every time loop entered ++attempt
-	//boolean flag needs to be set to true when all conditions happen
-	//start boolean flag at false , then check for username
-	//if username else-> error username (sucess false)
-	//if username true ->sucess
-	//iff psswd else->error 
-	// loop while attempt<=3
-	//then enter if startement with boolean
-	//can exit loop when boolean flag is true for both 
-	
-	System.out.println("Enter your password:");
-	passwordAttempt= input.next();
-	
-	if ((correctStoredUsername.equals(usernameAttempt))&&(correctStoredPassword.equals(passwordAttempt)))
-	{
-		System.out.println("Login Succesful! Welcome.");
-	}
-	else if ((correctStoredPassword.equals(passwordAttempt)&&(correctStoredUsername!=usernameAttempt)))
-	{
-		System.out.println("Incorrect username entered.");
-	}
-	else if ((correctStoredUsername.equals(usernameAttempt)&&(correctStoredPassword!=passwordAttempt)))
-	{
-		System.out.println("Incorrect password entered.");
-	}
-	else {
-		System.out.println("Incorrect login entered.");
-		//NEED TO SEPARATE, CANNOT GET TO PASSWORD WITHOUT HAVING A CORRECT USERNAME 
-	}
-	
-	 input.close();
-
 	}//end main
 
 }//end class
