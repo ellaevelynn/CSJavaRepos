b36d1a53cc58810bf248a48c4b35bb6f4e82cab1
ellaevelynn <ieaton@msudenver.edu>
2026-03-12 14:33:38 -0600

    Update LabL15ArraysIntro.java

--- EclipseWorkSpace/CS1050M04/LabL15ArraysIntro.java
+++ EclipseWorkSpace/CS1050M04/LabL15ArraysIntro.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class LabL15ArraysIntro
 {//start class
@@ -19,16 +20,31 @@ public class LabL15ArraysIntro
 	finalGrades = new double[numberStudents];
 	firstNames = new String[numberStudents];
 //Create a loop to have the user enter each name and finalGrade and store in the parallel arrays. 
-//No need to validate the score range.
-// enter the names for students first, then enter grades for each student
-	System.out.print("Enter the first names of each student in your class ");
+// No need to validate the score range.
+// Enter the names for students. print names to double check for accuracy
+	System.out.println("Enter the first names of each student in your class ");
 	for (int index = 0; index < numberStudents; index++) {//start loop
       firstNames[index] = input.next();
     }//end loop
-	System.out.print("the names of your students are "+firstNames[index]);
-//Challenge: Create a method where you pass both arrays.
+	System.out.println("the students you entered are: " +Arrays.toString(firstNames));
+//Enter grades for each student. print grades to check for accuracy
+	System.out.println("Now, enter the grades for each student in your class");
+	for (int index = 0; index < numberStudents; index++) {//start loop
+		System.out.println("Enter the final grade for: "+firstNames[index]);
+	      finalGrades[index] = input.nextDouble();
+	}//end loop
+	System.out.println("the grades for  " +Arrays.toString(firstNames)+ " are "+Arrays.toString(finalGrades));
+
 //Create a loop to find the sum of the final grades.
+	double sumGrades=0;
+	for (int index = 0; index < numberStudents; index++) {
+	    sumGrades= sumGrades+finalGrades[index];
+	    }
+	System.out.println("sum is " + sumGrades); //test for accuracy in adding all grade scores up
+	
 //Find the average of the final grades.
+	double averageGrade = sumGrades / numberStudents;
+	System.out.println("class average is " + averageGrade);
 //Create a method to display all names and grades of students that scored above the average.
 //Pass the two arrays to the method
 
