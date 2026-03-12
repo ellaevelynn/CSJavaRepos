954f9f7664031dc24acbfc091734e48ecc187291
ellaevelynn <ieaton@msudenver.edu>
2026-03-12 13:42:21 -0600

    editing in class

--- EclipseWorkSpace/CS1050M04/LabL15ArraysIntro.java
+++ EclipseWorkSpace/CS1050M04/LabL15ArraysIntro.java
@@ -1,20 +1,37 @@
-/**
- * 
- */
 
-/**
- * 
- */
 public class LabL15ArraysIntro
-{
+{//start class
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args)
-	{
-		// TODO Auto-generated method stub
+	{//start main
 
-	}
+//Declare an array called finalGrades to hold doubles 
+		double[] finalGrades;
+//Declare an array called firstNames to hold Strings.
+		String[] firstNames;
+//ask the user how many students are in the class
+	java.util.Scanner input = new java.util.Scanner(System.in);
+	System.out.println("enter the number of students in the class");
+	int numberStudents = input.nextInt();
+	System.out.println("You have "+numberStudents+" students in your class.");
 
-}
+//construct finalGrades and firstNames array using the number of students as the size 
+	finalGrades = new double[numberStudents];
+	firstNames = new String[numberStudents];
+//Create a loop to have the user enter each name and finalGrade and store in the parallel arrays. 
+//No need to validate the score range.
+// enter the names for students first, then enter grades for each student
+	System.out.print("Enter the first names of each student in your class ");
+	for (int index = 0; index < numberStudents; index++) {//start loop
+      firstNames[index] = input.next();
+    }//end loop
+	System.out.print("the names of your students are "+firstNames[index]);
+//Challenge: Create a method where you pass both arrays.
+//Create a loop to find the sum of the final grades.
+//Find the average of the final grades.
+//Create a method to display all names and grades of students that scored above the average.
+//Pass the two arrays to the method
+
+	}//end main
+
+}//end class
