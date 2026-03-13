1b4babe78297330f058fd3c0ace8583623629934
ellaevelynn <ieaton@msudenver.edu>
2026-03-13 17:30:52 -0600

    Update IEGE03CalculateSum.java

--- EclipseWorkSpace/CS1050M03/IEGE03CalculateSum.java
+++ EclipseWorkSpace/CS1050M03/IEGE03CalculateSum.java
@@ -4,6 +4,7 @@
  *  Description: Guided Exploration 03     
  *  The program will find the sum of the maximum of two values.
  */
+import java.awt.im.spi.InputMethodDescriptor;
 import java.util.Scanner;
 public class IEGE03CalculateSum
 {
@@ -14,11 +15,13 @@ public class IEGE03CalculateSum
 	int numberOne=input.nextInt();
 	System.out.println("Please enter another number");
 	int numberTwo=input.nextInt();
-	System.out.print("the numbers you entered are "+numberOne+" and "+numberTwo);
+	System.out.println("the numbers you entered are "+numberOne+" and "+numberTwo);
 	//checking that the values were stored properly
 	int maximumValue=FindMax(numberOne,numberTwo);
-	System.out.print("the maximum value between the two numbers is " + maximumValue);
-	
+	System.out.println("the maximum value between the two numbers is " + maximumValue);
+	int sumMaxValue=CalculateSum(maximumValue);
+	System.out.println("the sum of " + maximumValue+" is "+ sumMaxValue);
+	input.close();
 	//method one: determining the maximum value and replacing 
 	}//end main method
 	public static int FindMax(int numberOne,int numberTwo) {
@@ -33,9 +36,12 @@ public class IEGE03CalculateSum
 	}//end method one: finding max value
 
 	//method two: finding the sum of the maximum value 
-	public static int FindSum(int maximumValue) {
-	sum=
-		return sum;
+	public static int CalculateSum(int maximumValue) {
+	int sumMaxValue=0;
+	for (int integerCounter = 0; integerCounter <= maximumValue; integerCounter++) {
+			      sumMaxValue = sumMaxValue+ integerCounter;
+			    }
+		return sumMaxValue;
 	}//end method 2 
 	
 }//end class
