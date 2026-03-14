ab6362661955f721b0510a21f32609132446e145
ellaevelynn <ieaton@msudenver.edu>
2026-03-13 21:28:10 -0600

    Update IEGE03TrainerProject.java
    Method: AskToContinue

--- EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
+++ EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
@@ -11,12 +11,14 @@ public class IEGE03TrainerProject
 
 	public static void main(String[] args)
 	{//start main
-	
-	//display story 1
-	PrintSummary();
-	
 	//setup scanner for inputs 
 	java.util.Scanner methodInput = new java.util.Scanner(System.in);
+	//counter for story 5
+	int athleteEntriesCount = 0;
+	boolean keepCalculatingBMI = true;
+	//display story 1
+	PrintSummary();
+	do {
 	
 	//display user story 2 
 	String questionPrompt="placeholder";
@@ -26,12 +28,21 @@ public class IEGE03TrainerProject
 	
 	//display user story 3
 	double athleteBmiValue=calculateBMI(athleteWeight,athleteHeight);
-	System.out.println("\n The BMI of this Athlete is  "+athleteBmiValue);
+	System.out.println("\n The BMI of this Athlete is:  "+athleteBmiValue);
 	
 	//display user story 4
 	String bmiCategory=getBMICategory(athleteBmiValue);
-	System.out.println("\n The BMI category of this Athlete is  "+bmiCategory);
+	System.out.println("\n The BMI category of this Athlete is:  "+bmiCategory);
 	
+	//display user story 5
+	
+	//keep track of bmi for athletes entered 
+	athleteEntriesCount++;
+	keepCalculatingBMI = AskToContinue(methodInput);
+		} while (keepCalculatingBMI);
+	System.out.println("\n" + athleteEntriesCount + " athlete calculations were completed.");
+	methodInput.close();
+	   
 	}//end main
 	
 //User story 1: Method to display summary.
@@ -84,4 +95,19 @@ public class IEGE03TrainerProject
 		return bmiCategory;
 
 	}//end method 4/ user story 4 
+
+//User Story 5: Method: AskToContinue
+	public static boolean AskToContinue(java.util.Scanner methodInput) {
+	char userCharInput='z';
+	boolean isValidChar=false;
+	while (isValidChar==false) {
+	System.out.println("would you like to calculate the BMI for another athlete? (y or n)");
+	userCharInput = methodInput.next().toLowerCase().charAt(0);
+	if (userCharInput == 'y' || userCharInput == 'n') {
+        isValidChar = true;// queue to exit loop 
+	}
+	}
+	return userCharInput == 'y';
+	}//end method 5/ user story 5 
+	
 }//end class
