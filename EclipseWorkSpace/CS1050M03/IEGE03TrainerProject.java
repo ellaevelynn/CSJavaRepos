ed4ce3b06ebf8a1aeb33fa09b6811df5d405c110
ellaevelynn <ieaton@msudenver.edu>
2026-03-13 18:04:29 -0600

    editing ge03

---
+++ EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
@@ -0,0 +1,18 @@
+/*
+ *  Name: Isabella Eaton
+ *  Class: CS1050-004 (T/TH)
+ *  Description: Guided Exploration 03     
+ *  The program will provide a summary of a BMI analysis program.
+ */
+public class IEGE03TrainerProject
+{//start class
+PrintSummary();
+	
+	public static void main(String[] args)
+	{//start main
+		
+
+	}//end main
+	public static void PrintSummary() {
+}//end PrintSummary
+}//end Class
---
+++ EclipseWorkSpace/CS1050M04/IEGE03AgileDevelopment.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class IEGE03AgileDevelopment
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
