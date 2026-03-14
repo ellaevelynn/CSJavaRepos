e5e69f9d29cc3953fb6d9d188a31a6a77329db1e
ellaevelynn <ieaton@msudenver.edu>
2026-03-13 18:42:19 -0600

    Update IEGE03TrainerProject.java
    step 1: method for printing overview

--- EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
+++ EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
@@ -6,13 +6,23 @@
  */
 public class IEGE03TrainerProject
 {//start class
-PrintSummary();
-	
+
 	public static void main(String[] args)
 	{//start main
-		
+	PrintSummary();
 
 	}//end main
 	public static void PrintSummary() {
+	System.out.print("Team and Athlete Analysis\n"
+			+ "The trainer enters athlete data until they indicate they are done entering data."
+			+ "\nThe trainer will enter each athlete’s weight and height"
+			+ "\nFor each athlete entered, the BMI value and category will be displayed based"
+			+"\non these BMI ranges.\n"
+			+ "Under 18.5: Underweight\n"
+			+ "18.5 to under 25: Normal\n"
+			+ "25 to under 30: Overweight\n"
+			+ "30 or greater: Obese"
+				);
+	
 }//end PrintSummary
 }//end Class
