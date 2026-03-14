62cdc0bccbccca72906ed400b3ed18c077d1722a
ellaevelynn <ieaton@msudenver.edu>
2026-03-13 21:01:21 -0600

    Update IEGE03TrainerProject.java
    Method to Calculate BMI

--- EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
+++ EclipseWorkSpace/CS1050M03/IEGE03TrainerProject.java
@@ -20,13 +20,18 @@ public class IEGE03TrainerProject
 	
 	//display user story 2 
 	String questionPrompt="placeholder";
-	double athleteWeight=GetPositiveDouble(methodInput,"\n Enter athlete Weight in pounds: ");
-	double athleteHeight=GetPositiveDouble(methodInput,"\nEnter Athlete Height in inches: ");
+	double athleteWeight=getPositiveDouble(methodInput,"\n Enter athlete Weight in pounds: ");
+	double athleteHeight=getPositiveDouble(methodInput,"\nEnter Athlete Height in inches: ");
 	System.out.println("Athlete information saved as:  " + athleteWeight + " lbs, " + athleteHeight + " inches.");
 	
 	//display user story 3
-	double athleteBmiValue=CalculateBMI(athleteWeight,athleteHeight);
-	System.out.println("The BMI of this Athlete is  "+athleteBmiValue);
+	double athleteBmiValue=calculateBMI(athleteWeight,athleteHeight);
+	System.out.println("\n The BMI of this Athlete is  "+athleteBmiValue);
+	
+	//display user story 4
+	String bmiCategory=getBMICategory(athleteBmiValue);
+	System.out.println("\n The BMI category of this Athlete is  "+bmiCategory);
+	
 	}//end main
 	
 //User story 1: Method to display summary.
@@ -44,7 +49,7 @@ public class IEGE03TrainerProject
 }//end PrintSummary/user story 1 
 	
 //User story 2: Method to Validate a Positive Double.
-	public static double GetPositiveDouble(java.util.Scanner methodInput, String questionPrompt){//need to use java.util.Scanner instead of just Scanner 
+	public static double getPositiveDouble(java.util.Scanner methodInput, String questionPrompt){//need to use java.util.Scanner instead of just Scanner 
 	double userDoubleInput=0;
 	boolean isValidDouble=false;
 	while (isValidDouble==false){
@@ -57,11 +62,26 @@ public class IEGE03TrainerProject
 	return userDoubleInput;
 	}//end method 2 / user story 2
 
-//User story 3:
-	public static double CalculateBMI(double athleteWeight, double athleteHeight) {
+//User story 3:Method to Calculate BMI
+	public static double calculateBMI(double athleteWeight, double athleteHeight) {
 	final double BMI_FORMULA_CONSTANT=703;
 	double athleteBmiFormula=(athleteWeight/(Math.pow(athleteHeight,2)))* BMI_FORMULA_CONSTANT;
 	return Math.round(athleteBmiFormula);
-	}
-	
+	}//end method 3/ user story 3 
+
+//User story 4:Get BMI Category
+	public static String getBMICategory(double athleteBmiValue ) {
+		String bmiCategory="Placeholder";
+		if (athleteBmiValue >= 30.0){
+			bmiCategory = "Obese";
+		} else if (athleteBmiValue >=25.0 ){
+			bmiCategory = "Overweight";
+		} else if (athleteBmiValue>= 18.5){
+			bmiCategory = "Healthy weight";
+		} else {
+			bmiCategory = "Underweight, needs review";
+		} 
+		return bmiCategory;
+
+	}//end method 4/ user story 4 
 }//end class
