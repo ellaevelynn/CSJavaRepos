406474cad2e98d1291d905e132778d626b9a6bbc
ellaevelynn <ieaton@msudenver.edu>
2026-04-01 15:33:03 -0600

    Update M04PassArraysLab.java

--- EclipseWorkSpace/CS1050M04/M04PassArraysLab.java
+++ EclipseWorkSpace/CS1050M04/M04PassArraysLab.java
@@ -4,7 +4,6 @@
 
 public class M04PassArraysLab
 {
-
 	public static void main(String[] args)
 	{
 
@@ -31,8 +30,134 @@ public class M04PassArraysLab
 		displayArray(array1);
 		System.out.println("Display array2\n");
 		displayArray(array2);
-	
-	
-	
+		
+		array1[1] = 9000;
+		array2[3] = 1000;
+
+		System.out.println("Display array1\n");
+		displayArray(array1);
+		System.out.println("Display array2\n");
+		displayArray(array2);
+
+		// Summarize part 1
+
+		// Part 2 Copy Array 1 one element at a time into arrayCopy
+		System.out.println("-------- Part 2 Copy with For Loop ---------");
+
+		int[] arrayCopy = new int[4];
+
+		System.out.println("\nreference variable array1 " + array1);
+		System.out.println("reference variable arrayCopy " + arrayCopy);
+
+		System.out.println("Display array1\n");
+		displayArray(array1);
+		System.out.println("Display arrayCopy\n");
+		displayArray(arrayCopy);
+
+		for (int i = 0; i < array1.length; i++)
+		{
+			arrayCopy[i] = array1[i];
+		}
+
+		array1[1] = -400;
+		arrayCopy[3] = -999;
+
+		System.out.println("\nreference variable array1 " + array1);
+		System.out.println("reference variable arrayCopy " + arrayCopy);
+
+		System.out.println("Display array1\n");
+		displayArray(array1);
+		System.out.println("Display arrayCopy\n");
+		displayArray(arrayCopy);
+
+		// Summarize part 2
+
+		// Part 3 Passing array3 into updatePassedArray Method
+		System.out.println("-------- Part 3 Pass Arrays---------");
+
+		int[] array3 =
+		{ 100, 200, 300, 400 };
+
+		System.out.println("\nreference variable array3 " + array3);
+
+		System.out.println("Display array3\n");
+		displayArray(array3);
+
+		updatePassedArray(array3);
+
+		System.out.println("Display array3\n");
+		displayArray(array3);
+
+		// summarize Part 3
+
+		// Part 4 Returning arrays from a method
+		System.out.println("-------- Part 4 Return Arrays from method---------");
+
+		System.out.println("call createArray method\n");
+
+		int[] array4 = createArray(5);
+
+		System.out.println("after createArray method");
+		System.out.println("reference variable array4 " + array4);
+		System.out.println("Display array4\n");
+		displayArray(array4);
+
+	}// end main
+
+	/*
+	 * 
+	 */
+	public static void updatePassedArray(int passedArray[])
+	{
+		System.out.println("\n** STACK updatePassedArray method start **");
+		System.out.println("passedArray " + passedArray);
+
+		for (int i = 0; i < passedArray.length; i++)
+		{
+
+			passedArray[i] = passedArray[i] + 100;
+		}
+
+		// Notice you can call a method from with in this method to display passedArray
+		displayArray(passedArray);
+		System.out.println("** STACK updatePassedArray method end**\n");
+
 	}
-}
+
+	/*
+	 * 
+	 */
+	public static int[] createArray(int arraySize)
+	{
+		System.out.println("\n** STACK createArray method start** ");
+
+		int[] newArray = new int[arraySize];
+		System.out.println("createArray " + newArray);
+		for (int i = 0; i < newArray.length; i++)
+		{
+
+			newArray[i] = (int) (Math.random() * 10 + 1);
+		}
+		System.out.println("** STACK createArray method end ** \n");
+
+		return newArray;
+	}
+
+	/*
+	 * 
+	 */
+	public static void displayArray(int passedArray[])
+	{
+		System.out.println("\n** STACK displayArray method start** ");
+		System.out.println("passedArray " + passedArray);
+
+		for (int i = 0; i < passedArray.length; i++)
+		{
+
+			System.out.printf("array[%d] = %d \n", i, passedArray[i]);
+		}
+		System.out.println("** STACK displayArray method end** \n");
+	}
+
+}// end of class
+	
