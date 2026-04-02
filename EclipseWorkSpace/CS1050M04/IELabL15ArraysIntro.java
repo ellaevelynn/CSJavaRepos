8e5fc1c39355d8e8a4a3b0e184766dde096d88d3
ellaevelynn <ieaton@msudenver.edu>
2026-04-01 20:05:11 -0600

    editing

---
+++ EclipseWorkSpace/CS1050M04/IELabL15ArraysIntro.java
@@ -0,0 +1,81 @@
+/*
+ *  Name: Isabella Eaton
+ *  Class: CS1050-004 (T/TH)
+ *  Description: Guided Exploration 04   
+ *  The program will get the final grades and first names of students. Then find the average and display all students that were above the average. 
+ */
+public class IELabL15ArraysIntro
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+	java.util.Scanner userKeyboardInput = new java.util.Scanner(System.in);
+	//1. Declare an array called finalGrades to hold doubles.
+	double[] finalGrades;// no memory stored yet or size of array
+		
+	//2. Declare an array called firstNames to hold Strings.
+		String[]firstNames;// no memory stored yet or size of array
+		
+	//2. Ask the user how many student in the class
+	int numberStudents=0;
+	System.out.println("Please enter the number of students in your class:");
+	numberStudents = userKeyboardInput.nextInt();
+		
+	//3. Construct the finalGrades and firstNames array using the number of students as the size
+	finalGrades=new double [numberStudents];
+	firstNames=new String [numberStudents];
+		
+	//method to pass arrays and scanner to be able to fill in arrays with user input together 
+	fillStudentData(firstNames,finalGrades,userKeyboardInput);
+	
+	
+	//getting averages from students (create a method L16 lab)
+	double finalGradeAverage=calculateAverage(finalGrades);
+	System.out.println("the class average for the final is: "+finalGradeAverage);
+	
+	//method to display students above average for the final 
+	displayAboveAverage(firstNames, finalGrades, finalGradeAverage);
+    userKeyboardInput.close();//avoid data leak risk by closing keyboard input 
+	}	//end main method  
+    
+	//4. Create a method to have the user enter each name and finalGrade and store in the parallel arrays. 
+		//No need to validate the score range.
+		
+	public static void fillStudentData(String[] firstNames, double[] grades, java.util.Scanner input) {
+	for (int i = 0; i < firstNames.length; i++) {
+	System.out.print("\nEnter student first name # " + (i + 1) + ": ");//i + 1 takes into account for index starting at 0 (less confusing for user)
+	firstNames[i] = input.next();
+	System.out.print("Enter final Grade for " + firstNames[i] + ": ");
+	grades[i] = input.nextDouble();
+	 }//end for loop
+	}//end method
+
+		
+	//5&6. Find the sum and average of the final grades. update: create a method for the sum and average
+	public static double calculateAverage (double[] finalGrades) {
+	double sumFinalGrades=0;
+	for (int integerCounter = 0; integerCounter < finalGrades.length; integerCounter++) {
+	sumFinalGrades +=finalGrades[integerCounter];
+	}
+	System.out.println("the sum of final grades is: "+sumFinalGrades );
+	double finalGradeAverage= (double)sumFinalGrades/finalGrades.length;
+	System.out.println(" the class average for final grades is: "+finalGradeAverage);
+	return finalGradeAverage;
+	}
+	
+	//7. Create a method to display all names and grades of students that scored above the average.
+		//Pass the two arrays to the method
+	public static void displayAboveAverage (String[] firstNames, double[] finalGrades, double finalGradeAverage) {
+		System.out.println("\nStudents who scored above the average:");
+	    for (int i = 0; i < finalGrades.length; i++) {
+	        // Checking if the current student's grade is higher than the average
+	        if (finalGrades[i] > finalGradeAverage) {
+	            System.out.println(firstNames[i] + ": " + finalGrades[i]);
+	        }// end if statement
+	    }//end for loop
+	    }	//end method
+}//end class
+
--- EclipseWorkSpace/CS1050M04/M04PassArraysLab.java
+++ EclipseWorkSpace/CS1050M04/M04PassArraysLab.java
@@ -27,63 +27,66 @@ public class M04PassArraysLab
 		array2[2] = 82; //setting the third index value to 82 in array2, stored in memory
 
 		System.out.println("Display array1\n");
-		displayArray(array1);
+		displayArray(array1);//calls method displAyarray using the index inputs for array1
 		System.out.println("Display array2\n");
-		displayArray(array2);
+		displayArray(array2);//calls method displAyarray using the input of the entries of array2
 		
-		array1[1] = 9000;
-		array2[3] = 1000;
+		array1[1] = 9000; //changes second index of array 1 to value 9000 from 112
+		array2[3] = 1000;//changes fourth index of array 3 to 1000 from 0
 
 		System.out.println("Display array1\n");
-		displayArray(array1);
+		displayArray(array1);//follows method for displayArray again, method uses updated array1
 		System.out.println("Display array2\n");
-		displayArray(array2);
+		displayArray(array2);//follows method for displayArray again, method uses updated array2
 
 		// Summarize part 1
+		//part one declares and creates array 1 and array 2, then uses the methods displayArray to print out the entries of each array and their index location.
 
 		// Part 2 Copy Array 1 one element at a time into arrayCopy
 		System.out.println("-------- Part 2 Copy with For Loop ---------");
 
-		int[] arrayCopy = new int[4];
+		int[] arrayCopy = new int[4];//declares and creates a 4 entry long int array called arrayCopy. default value for array is 0000
 
-		System.out.println("\nreference variable array1 " + array1);
-		System.out.println("reference variable arrayCopy " + arrayCopy);
+		System.out.println("\nreference variable array1 " + array1); //shows reference code for this variable
+		System.out.println("reference variable arrayCopy " + arrayCopy); //shows reference memory for this variable
 
 		System.out.println("Display array1\n");
-		displayArray(array1);
+		displayArray(array1);//uses displayArray method to print out index locations and values for each entry of this array
 		System.out.println("Display arrayCopy\n");
-		displayArray(arrayCopy);
+		displayArray(arrayCopy);//uses displayArray method to print out index locations and values for each entry of this array
 
-		for (int i = 0; i < array1.length; i++)
+
+		for (int i = 0; i < array1.length; i++) //uses array 1, starts at the first index value and completes once it reaches the end of the array.
 		{
-			arrayCopy[i] = array1[i];
+			arrayCopy[i] = array1[i];//each entry for array copy will be assigned the entries of array1 at their shared indices.
 		}
 
-		array1[1] = -400;
-		arrayCopy[3] = -999;
+		array1[1] = -400;//after completing the for loop, array 1 is then altered at the second index
+		arrayCopy[3] = -999;//after completing the for loop, the 4th index entry of this array is altered
 
-		System.out.println("\nreference variable array1 " + array1);
-		System.out.println("reference variable arrayCopy " + arrayCopy);
+		System.out.println("\nreference variable array1 " + array1);//shows reference memory for this variable
+		System.out.println("reference variable arrayCopy " + arrayCopy);//shows reference memory for this variable
 
 		System.out.println("Display array1\n");
-		displayArray(array1);
+		displayArray(array1);//uses displayArray method to print out index locations and values for each entry of this array
 		System.out.println("Display arrayCopy\n");
-		displayArray(arrayCopy);
+		displayArray(arrayCopy);//uses displayArray method to print out index locations and values for each entry of this array
 
 		// Summarize part 2
+		//part two creates a new array called copyArray that starts out as an array of default values, it then goes thorugh a for loop where each index value of the new array is assigned the same values that are in array1. FInally, array1 and arrayCopy receive edits to their entries after completing the for loop
 
 		// Part 3 Passing array3 into updatePassedArray Method
 		System.out.println("-------- Part 3 Pass Arrays---------");
 
 		int[] array3 =
-		{ 100, 200, 300, 400 };
+		{ 100, 200, 300, 400 };//array 3 is declared and created with non-default values.
 
 		System.out.println("\nreference variable array3 " + array3);
 
 		System.out.println("Display array3\n");
-		displayArray(array3);
+		displayArray(array3);//displayArraymethod used again
 
-		updatePassedArray(array3);
+		updatePassedArray(array3);//different method referenced using entries of array3
 
 		System.out.println("Display array3\n");
 		displayArray(array3);
@@ -95,7 +98,7 @@ public class M04PassArraysLab
 
 		System.out.println("call createArray method\n");
 
-		int[] array4 = createArray(5);
+		int[] array4 = createArray(5);//new array is declared and created with default values, it is 5 entries long.
 
 		System.out.println("after createArray method");
 		System.out.println("reference variable array4 " + array4);
@@ -107,57 +110,58 @@ public class M04PassArraysLab
 	/*
 	 * 
 	 */
-	public static void updatePassedArray(int passedArray[])
+	public static void updatePassedArray(int passedArray[])//void, returns nothing to main
 	{
-		System.out.println("\n** STACK updatePassedArray method start **");
+		System.out.println("\n** STACK updatePassedArray method start **");//prints before method continues
 		System.out.println("passedArray " + passedArray);
 
-		for (int i = 0; i < passedArray.length; i++)
+		for (int i = 0; i < passedArray.length; i++)//follows the array specified in main when referencing the method, goes from fromt to end of array.
 		{
 
-			passedArray[i] = passedArray[i] + 100;
+			passedArray[i] = passedArray[i] + 100;//for each index entry of the array, it adds 100 to whatever value is at that index for the array, because method is void, these changed index values only occur in this method, it is not returned back to the array in main
+
 		}
 
 		// Notice you can call a method from with in this method to display passedArray
-		displayArray(passedArray);
+		displayArray(passedArray);//the updated array is displayed, but not saved to main, the array is only changed in this one instance, and then memory is removed.
 		System.out.println("** STACK updatePassedArray method end**\n");
 
-	}
+	}//end method
 
 	/*
 	 * 
 	 */
-	public static int[] createArray(int arraySize)
+	public static int[] createArray(int arraySize)//returns an int
 	{
 		System.out.println("\n** STACK createArray method start** ");
 
-		int[] newArray = new int[arraySize];
+		int[] newArray = new int[arraySize];//declares and creates an array of the size that is input in the method parameters
 		System.out.println("createArray " + newArray);
-		for (int i = 0; i < newArray.length; i++)
+		for (int i = 0; i < newArray.length; i++)//then follows a loop that assigns random values multiplied by 10 and +1 for each index of the created array
 		{
 
 			newArray[i] = (int) (Math.random() * 10 + 1);
 		}
 		System.out.println("** STACK createArray method end ** \n");
 
-		return newArray;
-	}
+		return newArray;//returns new array to main, returns index entries for the array to main.
+	}//end method
 
 	/*
 	 * 
 	 */
-	public static void displayArray(int passedArray[])
+	public static void displayArray(int passedArray[])//void means it returns nothing to main, uses array defined and stored in main
 	{
-		System.out.println("\n** STACK displayArray method start** ");
-		System.out.println("passedArray " + passedArray);
+		System.out.println("\n** STACK displayArray method start** ");//prints this message first every time this method is called
+		System.out.println("passedArray " + passedArray);//prints the reference code to the memory of this array
 
-		for (int i = 0; i < passedArray.length; i++)
+		for (int i = 0; i < passedArray.length; i++)// start at the beginning of the array index and loop ends when it reaches the last index
 		{
 
-			System.out.printf("array[%d] = %d \n", i, passedArray[i]);
+			System.out.printf("array[%d] = %d \n", i, passedArray[i]); //prints index location and the number stored at that location
 		}
-		System.out.println("** STACK displayArray method end** \n");
-	}
+		System.out.println("** STACK displayArray method end** \n");//signals that the method has ended, returns back to main method. does not return anything, only prints and returns to the main method.
+	}//end method
 
 }// end of class
 	
