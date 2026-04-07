f9f5421aafdbd6e9a482428623e7cf0ac13fdd9c
ellaevelynn <ieaton@msudenver.edu>
2026-04-07 11:29:00 -0600

    Create ProjectIteration01.java

---
+++ EclipseWorkSpace/CS1050M04/ProjectIteration01.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class ProjectIteration01
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
