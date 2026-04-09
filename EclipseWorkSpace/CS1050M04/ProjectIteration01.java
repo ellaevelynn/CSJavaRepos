bd385ec45e01a13bff4d292c7493d869d7e04453
ellaevelynn <ieaton@msudenver.edu>
2026-04-09 14:37:16 -0600

    Project Iteration 01: story 1
    Editing user story 1: print program overview for athlete training data.

--- EclipseWorkSpace/CS1050M04/ProjectIteration01.java
+++ EclipseWorkSpace/CS1050M04/ProjectIteration01.java
@@ -12,13 +12,14 @@ public class ProjectIteration01
 	public static void main(String[] args)
 	{//start main
 	//User Story 1: display program overview 
-	//call method in main 
+	//call method in main
 	displayProgramOverview();
 	
 	}//end main
 	//user story 1: method to display program overview 
+	//testing:
 	public static void displayProgramOverview() {
-	System.out.print("test");	}
+		System.out.println("Program Overview:");	}
 
 	
 }//end class
