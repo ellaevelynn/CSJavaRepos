43cfda85cd9b55238e96c2cdada13ac7ee85762c
ellaevelynn <ieaton@msudenver.edu>
2026-04-11 16:19:55 -0600

    editing project iteration 1
    user story 1: print program overview, edited text to actual text for assignment.

--- EclipseWorkSpace/CS1050M04/ProjectIteration01.java
+++ EclipseWorkSpace/CS1050M04/ProjectIteration01.java
@@ -19,7 +19,16 @@ public class ProjectIteration01
 	//user story 1: method to display program overview 
 	//testing:
 	public static void displayProgramOverview() {
-		System.out.println("Program Overview:");	}
+		System.out.println("-------- Program Overview ---------");
+		System.out.println("The trainer enters how many athletes are on the team.\n"
+				+ "Then the trainer enters each athlete's weigh, height, and age.\n"
+				+"For each athlete, the program calculates BMI and Max Heart Rate.\n"
+				+"BMI Categories:\n"
+				+ "Under 18.5: Underweight\n"
+				+ "18.5 to under 30: Normal\n"
+				+ "30 or greater: High\n"
+				+ "Calculates percentages of max heart rate for athlete training goal if needed.\n");
+	}//end method 
 
 	
 }//end class
