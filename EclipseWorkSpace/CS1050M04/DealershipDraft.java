a818876688a378c0bd1d448ee43874bf98973803
ellaevelynn <ieaton@msudenver.edu>
2026-04-21 13:48:46 -0600

    Update DealershipDraft.java

--- EclipseWorkSpace/CS1050M04/DealershipDraft.java
+++ EclipseWorkSpace/CS1050M04/DealershipDraft.java
@@ -4,5 +4,38 @@
 
 public class DealershipDraft
 {
+/**main method**/
+public static void main(String[] args) {
+	//create a car 
+	Car car1= new Car ("Toyota", "Prius", 20000.00);
+}//end main
+}//end class
 
+class Car{
+//initializing
+String make;
+String model;
+Double price;
+
+//getters and setters 
+/** construct an entry for a car**/
+Car(String make, String model, double price){
+this.make=make;
+	
+	String getMake() {
+	make=new make;
 }
+
+String getModel() {
+	model= new model;
+}
+
+double getPrice() {
+	price= new price;
+}
+void displayCarDetails() {
+	//printing something?
+}
+}//end get/set
+
+}//end car class
