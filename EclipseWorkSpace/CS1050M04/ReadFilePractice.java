15873c0e43b452431166361e18ca317f2990488a
ellaevelynn <ieaton@msudenver.edu>
2026-04-23 13:33:24 -0600

    Create ReadFilePractice.java

---
+++ EclipseWorkSpace/CS1050M04/ReadFilePractice.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class ReadFilePractice
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
