905f631a5c283731b6ed5a6654efb4db5bf3ea75
ellaevelynn <ieaton@msudenver.edu>
2026-06-03 10:02:29 -0600

    semester wrapup
    re-organizing files

---
+++ EclipseWorkSpace/cs2myFirstProject/src/cs2myFirstProject/HelloWorld_ISABELLA_EATON.java
@@ -0,0 +1,12 @@
+package cs2myFirstProject;
+
+public class HelloWorld_ISABELLA_EATON
+{
+
+	public static void main(String[] args)
+	{
+		System.out.print("Hello world. This is ISABELLA EATON'S first Java program");;
+
+	}
+
+}
